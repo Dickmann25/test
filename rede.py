a395aba36251ffe4ec01ae9a7f81fcecd819da24
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2023-12-04 12:20:42 -0300

    Create rede.py

---
+++ rede.py
@@ -0,0 +1,20 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSSwitch, RemoteController
+from mininet.cli import CLI
+
+class OpenFlowTopo(Topo):
+    def build(self):
+        s1 = self.addSwitch('s1', cls=OVSSwitch, protocols='OpenFlow13')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+
+if __name__ == '__main__':
+    topo = OpenFlowTopo()
+    net = Mininet(topo, controller=RemoteController, autoSetMacs=True)
+    net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+    net.start()
+    CLI(net)
+    net.stop()
