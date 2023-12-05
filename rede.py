91b689a0e2df3da501b7f7c24128b240c47a1c95
Dickmann25 <106775626+Dickmann25@users.noreply.github.com>
2023-12-05 13:04:05 -0300

    Update rede.py

--- rede.py
+++ rede.py
@@ -8,8 +8,12 @@ class OpenFlowTopo(Topo):
         s1 = self.addSwitch('s1', cls=OVSSwitch, protocols='OpenFlow13')
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
         self.addLink(h1, s1)
         self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s1)
 
 if __name__ == '__main__':
     topo = OpenFlowTopo()
